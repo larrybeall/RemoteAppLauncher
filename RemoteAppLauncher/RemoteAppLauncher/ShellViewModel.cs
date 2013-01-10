bcd43011a76789d3f63c5547fb16ae63595cb317
Larry Beall <lawrence.beall@gmail.com>
2013-01-10 10:54:34 -0500

    Initial Application Commit

---
+++ RemoteAppLauncher/RemoteAppLauncher/ShellViewModel.cs
@@ -0,0 +1,17 @@
+﻿using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RemoteAppLauncher
+{
+    public class ShellViewModel : Conductor<IScreen>.Collection.OneActive
+    {
+        public ShellViewModel()
+        {
+            DisplayName = "AppLauncher";
+        }
+    }
+}
