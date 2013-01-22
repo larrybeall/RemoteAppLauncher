64a4e164c60569a4269451cf7897032b79c9bf4c
Larry Beall <lawrence.beall@gmail.com>
2013-01-22 00:14:45 -0500

    License Change, Source Tree Cleanup, Tooltip styling, grouping main screen, fix unpin/pin behavior

--- RemoteAppLauncher/RemoteAppLauncher/Presentation/Converters/PinnedToGroupNameConverter.cs
+++ RemoteAppLauncher/RemoteAppLauncher/Presentation/Converters/PinnedToGroupNameConverter.cs
@@ -1,21 +1,20 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using System.Windows.Data;
 
 namespace RemoteAppLauncher.Presentation.Converters
 {
-    public class NotEqualNullConverter : IValueConverter
+    public class PinnedToGroupNameConverter : IValueConverter
     {
         public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
         {
-            if (value == null)
-                return false;
+            if (!(value is bool))
+                return value;
 
-            if (value is string)
-            {
-                return !string.IsNullOrEmpty((string)value);
-            }
-
-            return true;
+            return (bool) value ? "Pinned" : "Most Used";
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
